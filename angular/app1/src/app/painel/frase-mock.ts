bb139ca4b0d56a28a0b57ad783fd3ca8c8cfe565
mauricio.notebook <mauricioassisrt@gmail.com>
2023-02-21 16:31:15 -0300

    101

---
+++ angular/app1/src/app/painel/frase-mock.ts
@@ -0,0 +1,9 @@
+import { Frase } from "app/shared/frase.model";
+
+const FRASES: Frase[] = [
+  { fraseEng: 'I like to learn' , frasePtBr: 'Eu gosto de aprender'},
+  { fraseEng: 'I watch tv' , frasePtBr: 'Eu assisto tv'},
+  { fraseEng: 'How are you?' , frasePtBr: 'Como vai você?'},
+  { fraseEng: 'I eat bread' , frasePtBr: 'Eu como pão'}
+]
+
