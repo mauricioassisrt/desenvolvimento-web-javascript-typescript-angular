fb85deae3bd59c2c125dddc53a7ee0b18471923e
mauricio.notebook <mauricioassisrt@gmail.com>
2023-02-21 15:12:58 -0300

    89

--- angular/app1/src/app/app.module.ts
+++ angular/app1/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TopoComponent
+    TopoComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule
