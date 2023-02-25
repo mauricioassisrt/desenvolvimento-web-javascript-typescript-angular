a0eee09f770120dfef0fb0eea3aadf3f50e1d6f0
Mauricio de Assis <mauriciox8mm@hotmail.com>
2023-02-25 12:56:58 -0300

    113

--- angular/app1/src/app/painel/painel.component.ts
+++ angular/app1/src/app/painel/painel.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Frase } from 'app/shared/frase.model';
-import { FRASES } from './frase-mock';
+import {Component, OnInit} from '@angular/core';
+import {Frase} from 'app/shared/frase.model';
+import {FRASES} from './frase-mock';
 
 @Component({
   selector: 'app-painel',
@@ -10,11 +10,40 @@ import { FRASES } from './frase-mock';
 export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase'
+  public resposta: string = ''
+  public rodada: number = 0
+  public rodadaFrase: Frase
+  public progresso: number = 0
   constructor() {
-    console.log(this.frases)
-   }
+    this.atualizaRodada()
+  }
 
   ngOnInit() {
   }
 
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+
+  }
+
+  public verificarResposta(): void {
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      this.rodada++
+      this.atualizaRodada()
+      this.progresso += (100 / this.frases.length)
+      console.log(this.progresso)
+      alert('Tradução correta')
+    } else {
+      alert('Traducao errada')
+    }
+    /*trocar pergunta rodada*/
+    if (this.rodada == this.frases.length)
+        this.rodada = 0
+
+  }
+
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada]
+    this.resposta = ''
+  }
 }
