98ae95b8252df028fdc964d3d2068d86cff8ff63
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-01 13:08:56 -0300

    148

--- angular/app2/src/app/home/home.component.ts
+++ angular/app2/src/app/home/home.component.ts
@@ -17,8 +17,11 @@ export class HomeComponent implements OnInit {
     // this.ofertas = this.ofertaService.getOfertas()
     // console.log(this.ofertas)
     this.ofertaService.getOfertas2()
-      .then((ofertas: Oferta[] ) => {   this.ofertas = ofertas},
-        (param: any)=> console.log(param))
+      .then((ofertas: Oferta[] ) => {
+        this.ofertas = ofertas})
+      .catch((param: any)=> {
+        console.log(param)
+      })
   }
 
 }
