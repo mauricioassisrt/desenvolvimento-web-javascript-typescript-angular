d96b13e6180fbf81c653096d2693e478a1531c68
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-01 11:04:38 -0300

    aula 143

--- angular/app2/src/app/home/home.component.ts
+++ angular/app2/src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {OfertaService} from "../services/oferta.service";
+import {Oferta} from "../../shared/ofertas.model";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertaService]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public ofertas: Oferta[]
+  constructor(private ofertaService: OfertaService) { }
 
   ngOnInit() {
+    this.ofertas = this.ofertaService.getOfertas()
+    console.log(this.ofertas)
   }
 
 }
