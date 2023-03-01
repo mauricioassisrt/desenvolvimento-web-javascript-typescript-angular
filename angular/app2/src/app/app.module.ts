2e810287e55904600966846fefa85e95a82fceac
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-01 15:12:18 -0300

    159

--- angular/app2/src/app/app.module.ts
+++ angular/app2/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { RodapeComponent } from './rodape/rodape.component';
 import {HttpModule} from "@angular/http";
 import {RestaurantesComponent} from "./restaurantes/restaurantes.component";
 import {DiversaoComponent} from "./diversao/diversao.component";
+import {RouterModule} from "@angular/router";
+import { ROUTES } from "./app.routes";
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import {DiversaoComponent} from "./diversao/diversao.component";
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
