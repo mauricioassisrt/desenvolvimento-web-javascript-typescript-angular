11ec6ab90a75561d6afbeeca3ff6ee9ad6688dc2
mauricio.notebook <mauricioassisrt@gmail.com>
2023-03-04 15:37:05 -0300

    175

--- angular/app2/src/app/ofertas/oferta.component.ts
+++ angular/app2/src/app/ofertas/oferta.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {OfertaService} from '../services/oferta.service';
+import {Oferta} from '../../shared/ofertas.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
+  providers: [OfertaService]
 })
 export class OfertaComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,
+              private ofertasService:OfertaService) {}
+  public oferta: Oferta
   ngOnInit() {
-   console.log(this.route.snapshot.params['id'])
+    this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
+      .then(( oferta: Oferta) => {
+          this.oferta = oferta
+    })
   }
 
 }
--- angular/app2/src/app/services/oferta.service.ts
+++ angular/app2/src/app/services/oferta.service.ts
@@ -2,18 +2,23 @@ import {Oferta} from "../../shared/ofertas.model";
 import {Http} from "@angular/http";
 import {Injectable} from "@angular/core";
 import 'rxjs/add/operator/toPromise'
+import {URL_API} from '../app.api';
 @Injectable()
 export class OfertaService {
   constructor(private http: Http) { }
-  private url: string = "http://localhost:3000/ofertas"
   public getOfertas(): Promise<Oferta[]> {
-    return  this.http.get(`${this.url}?destaque=true`)
+    return  this.http.get(`${URL_API}?destaque=true`)
       .toPromise()
       .then((resposta: any)=> resposta.json())
   }
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`${this.url}?categoria=${categoria}`)
+    return this.http.get(`${URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta.json())
+  }
+  public getOfertaPorId(id:number):Promise<Oferta>{
+      return this.http.get(`${URL_API}?id=${id}`)
+        .toPromise()
+        .then((resposta:any) => resposta.json().shift())
   }
 }
