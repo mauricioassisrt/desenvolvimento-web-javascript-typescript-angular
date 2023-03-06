3254122a72030db1885dbaa6b655bd0774b2341a
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-06 13:33:30 -0300

    189

--- angular/app2/src/app/ofertas/oferta.component.ts
+++ angular/app2/src/app/ofertas/oferta.component.ts
@@ -1,35 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {OfertaService} from '../services/oferta.service';
 import {Oferta} from '../../shared/ofertas.model';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx'
 import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
   providers: [OfertaService]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
 
   constructor(private route: ActivatedRoute,
               private ofertasService:OfertaService) {}
   public oferta: Oferta
+
+  private temObservableSubscription: Subscription
+  private temObservableTestSubscription: Subscription
+
   ngOnInit() {
     this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
       .then(( oferta: Oferta) => {
           this.oferta = oferta
     })
-    /* this.route.params.subscribe(
+  /*   this.route.params.subscribe(
       (parametro: any) => { console.log(parametro)},
       (error: any) => console.log(error),
-      () =>  console.log("processo concluido")) */
-      // let tempo = Observable.interval(2500)
+      () =>  console.log("processo concluido"))
+ */
+      let tempo = Observable.interval(2500)
 
-      // tempo.subscribe((intervalo: number) => {
-      //   console.log(intervalo)
-      // })
+      this.temObservableSubscription = tempo.subscribe((intervalo: number) => {
+         console.log(intervalo)
+      })
       //observavel
       let meuObservable = Observable.create((observer: Observer<string>)=>{
         observer.next('Primeiro evento da stream')
@@ -38,11 +44,15 @@ export class OfertaComponent implements OnInit {
       })
 
       /// observador
-      meuObservable.subscribe(
+     this.temObservableTestSubscription = meuObservable.subscribe(
         (resultado: any) => console.log(resultado + 10),
         (erro: string) => console.log(erro),
         ()=> console.log("Finalizado")
       )
   }
 
+  ngOnDestroy() {
+    this.temObservableSubscription.unsubscribe()
+    this.temObservableSubscription.unsubscribe()
+  }
 }
