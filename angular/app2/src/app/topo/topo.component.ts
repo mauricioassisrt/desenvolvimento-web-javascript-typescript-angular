03da7049566e4e8402a841d6746e48c0dc042f93
Mauricio de Assis <mauriciox8mm@hotmail.com>
2023-03-07 19:01:44 -0300

    215

--- angular/app2/src/app/topo/topo.component.ts
+++ angular/app2/src/app/topo/topo.component.ts
@@ -1,19 +1,19 @@
-import { Oferta } from './../../shared/ofertas.model';
-import { Observable } from 'rxjs/Observable';
-import { OfertaService } from './../services/oferta.service';
-import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import {Oferta} from './../../shared/ofertas.model';
+import {Observable} from 'rxjs/Observable';
+import {OfertaService} from './../services/oferta.service';
+import {Component, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
   styleUrls: ['./topo.component.css'],
-  providers: [ OfertaService ]
+  providers: [OfertaService]
 })
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
-  public ofertas2: Oferta[]
+
   private subjectPesquisa: Subject<string> = new Subject()
 
   constructor(
@@ -23,30 +23,23 @@ export class TopoComponent implements OnInit {
   ngOnInit() {
 
     this.ofertas = this.subjectPesquisa
-    .debounceTime(1000)
-    .distinctUntilChanged()
-    .switchMap((termo: string) => {
-      console.log("requisicao HTTP")
+      .debounceTime(1000)
+      .distinctUntilChanged()
+      .switchMap((termo: string) => {
 
-      if(termo.trim() === '') {
-        return Observable.of<Oferta[]>([])
-      }
-      return this.ofertaService.pesquisaOfertas(termo)
-    })
+        if (termo.trim() === '') {
+          return Observable.of<Oferta[]>([])
+        }
+
+        return this.ofertaService.pesquisaOfertas(termo)
+      })
     .catch((err: any) => {
-      console.error(err)
       return Observable.of<Oferta[]>([])
-    })
-
-    this.ofertas.subscribe((ofertas: Oferta[]) =>  {
-      this.ofertas2 = ofertas
     })
 
   }
 
   public pesquisa(termoBusca: string): void {
-    console.log('keyup caracter', termoBusca)
     this.subjectPesquisa.next(termoBusca)
-
   }
 }
