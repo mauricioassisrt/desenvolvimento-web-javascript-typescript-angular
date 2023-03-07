7510e08535eba1ef391c86b9857378ba2bdca941
Mauricio de Assis <mauriciox8mm@hotmail.com>
2023-03-07 18:55:19 -0300

    213

--- angular/app2/src/app/util/descricao.reduzida.pipe.ts
+++ angular/app2/src/app/util/descricao.reduzida.pipe.ts
@@ -2,9 +2,9 @@ import {Pipe, PipeTransform} from "@angular/core";
 
 @Pipe({name: 'descricaoReduzida'})
 export class DescricaoReduzidaPipe implements PipeTransform {
-  transform(value: string): string {
-    if (value.length > 15) {
-      return value.substr(0, 15) + '...'
+  transform(value: string, truncarEm: number, iniciarEm: number): string {
+    if (value.length > truncarEm) {
+      return value.substr(iniciarEm, truncarEm) + '...'
     }
     return value
   }
