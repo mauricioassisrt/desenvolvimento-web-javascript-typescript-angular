7af5af573a3712a0ec1262edb4ab48923726f257
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-08 15:53:09 -0300

    225

--- angular/app2/src/app/ordem-compra/ordem-compra.component.ts
+++ angular/app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -13,6 +13,16 @@ export class OrdemCompraComponent implements OnInit, OnDestroy {
   public numero: string = ''
   public complemento: string = ''
   public formaPagamento: string = ''
+  public enderecoValido: boolean
+  public complementoValido: boolean
+  public numeroValido: boolean
+  public formaPagamentoValido: boolean
+
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo: boolean = true
+
 
   constructor() {}
 
@@ -24,24 +34,43 @@ export class OrdemCompraComponent implements OnInit, OnDestroy {
   }
   public atualizaEndereco(endereco: string) {
     this.endereco = endereco
+    this.enderecoEstadoPrimitivo = false
+    if(this.endereco.length > 3) {
+      this.enderecoValido = true
+    }else {
+      this.enderecoValido = false
+    }
     console.log(this.endereco)
   }
 
   public atualizaNumero(numero: string) {
     this.numero = numero
-
+    this.numeroEstadoPrimitivo = false
+    if(this.numero.length > 0){
+      this.numeroValido = true
+    }else{
+      this.numeroValido = false
+    }
     console.log(this.numero)
   }
 
   public atualizaComplemento(complemento: string) {
     this.complemento = complemento
-
+    this.complementoEstadoPrimitivo = false
+    if(this.complemento.length > 0){
+      this.complementoValido = true
+    }
     console.log(this.complemento)
   }
 
   public atualizaFormaPagamento(formaPagamento: string) {
     this.formaPagamento = formaPagamento
-
+    this.formaPagamentoEstadoPrimitivo = false
+    if(this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true
+    }else {
+      this.formaPagamentoValido = false
+    }
     console.log(this.formaPagamento)
   }
 }
