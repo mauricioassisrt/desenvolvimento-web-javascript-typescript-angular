8791a45eae1d17fec4234393c2797c9742736d9f
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-09 14:13:41 -0300

    261

--- angular/app2/src/app/ofertas/oferta.component.ts
+++ angular/app2/src/app/ofertas/oferta.component.ts
@@ -1,21 +1,28 @@
+
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {OfertaService} from '../services/oferta.service';
 import {Oferta} from '../../shared/ofertas.model';
 import 'rxjs/Rx'
+import CarrinhoService from 'app/services/carrinho.service';
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
-  providers: [OfertaService]
+  providers: [OfertaService, CarrinhoService]
 })
 export class OfertaComponent implements OnInit, OnDestroy {
-
-  constructor(private route: ActivatedRoute,
-              private ofertasService:OfertaService) {}
   public oferta: Oferta
 
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService:OfertaService,
+    private carrinhoService: CarrinhoService
+  ) {}
+
+
   ngOnInit() {
+    console.log(this.carrinhoService.exibirItens())
     this.route.params.subscribe((parametros: Params) => {
       this.ofertasService.getOfertaPorId(parametros.id)
       .then(( oferta: Oferta) => {
