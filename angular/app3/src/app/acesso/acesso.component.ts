674fceec868ac9c7b6ac1aca4c1305c4c3f5f03f
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-10 11:37:36 -0300

    299

--- angular/app3/src/app/acesso/acesso.component.ts
+++ angular/app3/src/app/acesso/acesso.component.ts
@@ -1,26 +1,38 @@
- import {animate, Component, OnInit, state, style, transition, trigger} from '@angular/core';
+import {animate, Component, OnInit, state, style, transition, trigger} from '@angular/core';
 
 @Component({
-  selector: 'app-acesso',
-  templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css'],
-  animations: [
-      trigger('animacao-banner', [
-          state('criado', style({
-            opacity: 1
-          })),
-        transition('void => criado', [
-              style({opacity: 0, transform: 'translate(-50px, 0)'}),
-              animate('500ms 1s ease-in-out' )
-        ])
-      ])
-  ]
+    selector: 'app-acesso',
+    templateUrl: './acesso.component.html',
+    styleUrls: ['./acesso.component.css'],
+    animations: [
+        trigger('animacao-banner', [
+            state('criado', style({
+                opacity: 1
+            })),
+            transition('void => criado', [
+                style({opacity: 0, transform: 'translate(-50px, 0)'}),
+                animate('500ms 1s ease-in-out')
+            ])
+        ]),
+        trigger('animacao-painel', [
+            state('criado', style({
+                opacity: 1
+            })),
+            transition('void => criado', [
+                style({opacity: 0, transform: 'translate(50px, 0)'}),
+                animate('500ms 1s ease-in-out')
+            ])
+        ]),
+    ]
 })
 export class AcessoComponent implements OnInit {
-  public estadoBanner: string = 'criado'
-  constructor() { }
+    public estadoBanner: string = 'criado';
+    public estadoPainel: string = 'criado';
+    public cadastro: boolean = false
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
