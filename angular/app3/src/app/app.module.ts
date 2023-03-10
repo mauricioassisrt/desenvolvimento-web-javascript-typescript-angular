5fda718d04c6c34875dc919b951c7725d36d932a
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-10 10:06:17 -0300

    292

--- angular/app3/src/app/app.module.ts
+++ angular/app3/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AcessoComponent } from './acesso/acesso.component';
 import { BannerComponent } from './acesso/banner/banner.component';
 import { LoginComponent } from './acesso/login/login.component';
 import { CadastroComponent } from './acesso/cadastro/cadastro.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
