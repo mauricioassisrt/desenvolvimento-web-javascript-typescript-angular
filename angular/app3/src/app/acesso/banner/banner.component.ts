f963a2d91419444da20349af9bb7c54f3bc49b0b
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-10 11:07:03 -0300

    296

--- angular/app3/src/app/acesso/banner/banner.component.ts
+++ angular/app3/src/app/acesso/banner/banner.component.ts
@@ -1,28 +1,52 @@
 import {animate, Component, OnInit, state, style, transition, trigger} from '@angular/core';
+import {Imagem} from './imagem.model';
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css'],
-  animations: [
-      trigger('banner',[
-          state('escondido', style({
-            opacity: 0
-          })),
-          state('visivel', style({
-            opacity: 1
-          })),
-          transition('escondido <=> visivel', animate('1s ease-in', ))
-      ])
-  ]
+    selector: 'app-banner',
+    templateUrl: './banner.component.html',
+    styleUrls: ['./banner.component.css'],
+    animations: [
+        trigger('banner', [
+            state('escondido', style({
+                opacity: 0
+            })),
+            state('visivel', style({
+                opacity: 1
+            })),
+            transition('escondido <=> visivel', animate('1s ease-in',))
+        ])
+    ]
 })
 export class BannerComponent implements OnInit {
-  public estado: string = 'visivel'
-  constructor() { }
+    public estado: string = 'visivel';
+    public imagens: Imagem[] = [
+        {estado: 'visivel', url: '/assets/imagens/img_1.png'},
+        {estado: 'escondido', url: '/assets/imagens/img_2.png'},
+        {estado: 'escondido', url: '/assets/imagens/img_3.png'},
+        {estado: 'escondido', url: '/assets/imagens/img_4.png'},
+        {estado: 'escondido', url: '/assets/imagens/img_5.png'},
+    ]
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
-  public toogleEstadoAnimacao(): void {
-      this.estado = this.estado === 'visivel' ? 'escondido': 'visivel'
-  }
+    ngOnInit() {
+     setTimeout(() =>this.logicaRotacao(), 2000)
+    }
+
+    public logicaRotacao(): void {
+        let idX: number
+        console.log(this.imagens)
+        for (let i: number =0; i<=4; i++) {
+            if(this.imagens[i].estado === 'visivel') {
+                this.imagens[i].estado = 'escondido'
+                idX = i === 4 ? 0 : i + 1
+                break
+            }
+        }
+
+        this.imagens[idX].estado = 'visivel'
+
+        setTimeout(() =>this.logicaRotacao(), 2000)
+    }
+
 }
