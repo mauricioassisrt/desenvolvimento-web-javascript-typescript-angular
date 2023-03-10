738389f4bc8b653698aa94eef299e29e22b2a681
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-10 15:41:40 -0300

    317

--- angular/app3/src/app/app.module.ts
+++ angular/app3/src/app/app.module.ts
@@ -1,29 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AcessoComponent } from './acesso/acesso.component';
-import { BannerComponent } from './acesso/banner/banner.component';
-import { LoginComponent } from './acesso/login/login.component';
-import { CadastroComponent } from './acesso/cadastro/cadastro.component';
+import {AppComponent} from './app.component';
+import {AcessoComponent} from './acesso/acesso.component';
+import {BannerComponent} from './acesso/banner/banner.component';
+import {LoginComponent} from './acesso/login/login.component';
+import {CadastroComponent} from './acesso/cadastro/cadastro.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ReactiveFormsModule} from '@angular/forms';
 import {AutenticacaoService} from './acesso/autenticacao.service';
+import {HomeComponent} from './acesso/home/home.component';
+import {PublicacoesComponent} from './acesso/home/publicacoes/publicacoes.component';
+import {RouterModule} from '@angular/router';
+import {ROUTES} from './app.routes';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AcessoComponent,
-    BannerComponent,
-    LoginComponent,
-    CadastroComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
-  ],
-  providers: [AutenticacaoService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AcessoComponent,
+        BannerComponent,
+        LoginComponent,
+        CadastroComponent,
+        HomeComponent,
+        PublicacoesComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(ROUTES)
+    ],
+    providers: [AutenticacaoService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
