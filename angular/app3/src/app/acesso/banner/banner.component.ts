168b315248c4d932d62bce8686c4d5fcfe900f64
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-10 10:48:37 -0300

    294

--- angular/app3/src/app/acesso/banner/banner.component.ts
+++ angular/app3/src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {animate, Component, OnInit, state, style, transition, trigger} from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+      trigger('banner',[
+          state('escondido', style({
+            opacity: 0
+          })),
+          state('visivel', style({
+            opacity: 1
+          })),
+          transition('escondido <=> visivel', animate('1s ease-in', ))
+      ])
+  ]
 })
 export class BannerComponent implements OnInit {
-
+  public estado: string = 'visivel'
   constructor() { }
 
   ngOnInit() {
   }
-
+  public toogleEstadoAnimacao(): void {
+      this.estado = this.estado === 'visivel' ? 'escondido': 'visivel'
+  }
 }
